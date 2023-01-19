45f98631705ca425cfdbc66a9d13f8a53ecf95ee
datle2403 <lemanhdat2112@gmail.com>
2023-01-19 13:22:13 +0700

    first commit

---
+++ run_load_data.py
@@ -0,0 +1,13 @@
+from load_data import *
+from setting import params
+os.chdir("/Users/datle/Desktop/license_plate_detection")
+car, non_car= load_dataset()
+# car=car[:3]
+# non_car=non_car[:3]
+car_feature=extract_feature(car, params['color_space'])
+non_car_feature= extract_feature(non_car, params['color_space'])
+X,y= combine(car_feature, non_car_feature)
+sc, X_scaled= normalize(X)
+X_train, X_test, y_train, y_test= split(X_scaled, y)
+model= train_model(X_train, X_test, y_train, y_test)
+save_model('lp_detect.p', model, sc, params=params,y=y)

ac3bf3b35deea5c764706808a57bd6709ef39f9f
datle2403 <lemanhdat2112@gmail.com>
2023-01-19 13:49:31 +0700

    m

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 params = {}
 params['color_space'] = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 params['orient'] = 9  # HOG orientations
-params['pix_per_cell'] = 8  # HOG pixels per cell
+params['pix_per_cell'] = 4  # HOG pixels per cell
 params['cell_per_block'] = 2  # HOG cells per block
 params['hog_channel'] = 'ALL'  # Can be 0, 1, 2, or "ALL"
 params['spatial_size'] = (16, 16)  # Spatial binning dimensions
@@ -9,7 +9,7 @@ params['hist_bins'] = 16  # Number of histogram bins
 params['spatial_feat'] = True  # Spatial features on or off
 params['hist_feat'] = True  # Histogram features on or off
 params['hog_feat'] = True  # HOG features on or off
-params['size_of_window']=(64,64,3)
+params['size_of_window']=(108,36,3)
 params['test_size']=0.2
 
 
@@ -18,7 +18,7 @@ win_size['scale_0']=(375,1000,0.5)
 win_size['scale_1']=(375,1000,1.3)
 win_size['scale_2']=(375,1000,1.8)
 win_size['scale_3']=(375,1000,2.3)
-win_size['thresh']=2
+win_size['thresh']=4
 win_size['overlap_thresh']= 0.05
 # 0.5, 1.3
-win_size['use_scale']=(2,)
+win_size['use_scale']=(3,)

87e22edd11c2746078abe45cb430116d343499b2
datle2403 <lemanhdat2112@gmail.com>
2023-01-19 14:12:02 +0700

    m

--- setting.py
+++ setting.py
@@ -10,15 +10,15 @@ params['spatial_feat'] = True  # Spatial features on or off
 params['hist_feat'] = True  # Histogram features on or off
 params['hog_feat'] = True  # HOG features on or off
 params['size_of_window']=(108,36,3)
-params['test_size']=0.2
+params['test_size']=0.8
 
 
 win_size={}
 win_size['scale_0']=(375,1000,0.5)
-win_size['scale_1']=(375,1000,1.3)
-win_size['scale_2']=(375,1000,1.8)
-win_size['scale_3']=(375,1000,2.3)
-win_size['thresh']=4
+win_size['scale_1']=(375,1000,0.9)
+win_size['scale_2']=(375,1000,1.4)
+win_size['scale_3']=(375,1000,2.2)
+win_size['thresh']=100
 win_size['overlap_thresh']= 0.05
 # 0.5, 1.3
-win_size['use_scale']=(3,)
+win_size['use_scale']=(0,1,2,3)
